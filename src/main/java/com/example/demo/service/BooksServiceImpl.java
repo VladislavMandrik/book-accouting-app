5c842413c8772d78d7f2705c0eaec7758e47ac23
VladislavMandrik <76885776+VladislavMandrik@users.noreply.github.com>
2021-05-15 00:19:17 +0300

    fix application.properties, ElectronicBook. add Pagination, insert books. CustomWebSecurityConfigurerAdapter, MyBasicAuthenticationEntryPoint transferred in package config.

--- src/main/java/com/example/demo/service/BooksServiceImpl.java
+++ src/main/java/com/example/demo/service/BooksServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.demo.service;
 import com.example.demo.model.Book;
 import com.example.demo.repository.BooksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
